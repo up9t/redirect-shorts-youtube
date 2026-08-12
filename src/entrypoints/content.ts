7c21bce072ae4de8f83dc91acb4b049258b6d2da
up9t <up9t99@gmail.com>
2026-08-12 03:11:00 +0000

    Use a higher level abstraction API to have a wider compatibility and support Firefox for Android

--- src/entrypoints/content.ts
+++ src/entrypoints/content.ts
@@ -1,27 +1,27 @@
+function replaceURL(old: URL, id: string) {
+  const newUrl = new URL(old);
+  newUrl.pathname = "/watch";
+  newUrl.search = `?v=${id}`;
+  location.replace(newUrl);
+}
+
+function handleURLChange(currentUrl: URL) {
+  const paths = currentUrl.pathname.split("/").slice(1);
+  const id = paths[1];
+
+  if (paths[0] === "shorts" && id) {
+    replaceURL(currentUrl, id);
+  }
+}
+
 export default defineContentScript({
-  matches: ['*://*.youtube.com/*'],
-  main() {
-    function replaceURL(){
-      const old = new URL(location.href);
-      const paths = old.pathname.split("/").slice(1);
-      const id = paths[1];
+  matches: ["*://*.youtube.com/*"],
+  main(context) {
+    context.addEventListener(window, "wxt:locationchange", (event) => {
+      handleURLChange(event.newUrl);
+    });
 
-      if (paths[0] === "shorts" && id) {
-        const newUrl = new URL(old)
-        newUrl.pathname = "/watch";
-        newUrl.search = `?v=${id}`;
-        console.log("replacing url with a new one: ", newUrl);
-        location.replace(newUrl)
-      }
-    }
-
-    replaceURL();
-
-    addEventListener("yt-navigate-start", () => {
-      replaceURL();
-    })
-    addEventListener("yt-navigate-finish", () => {
-      replaceURL();
-    })
+    // trigger for the first time
+    handleURLChange(new URL(location.href));
   },
 });
--- src/entrypoints/popup/main.ts
+++ src/entrypoints/popup/main.ts
@@ -1,6 +1,6 @@
-import './style.css';
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <h1 style="font-size: 2rem;">Redirect Shorts on YouTube</h1>
   <p>Play YouTube shorts as a normal video to remove distraction and endless scrolling.</p>
 `;
